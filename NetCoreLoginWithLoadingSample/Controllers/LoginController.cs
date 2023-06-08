35c561b5e72d9d3e40012b3018b3aa4a2509d605
Alican Çevik <alican.cevik@hotmail.com>
2023-06-09 00:14:14 +0300

    Add project files.

---
+++ NetCoreLoginWithLoadingSample/Controllers/LoginController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetCoreLoginWithLoadingSample.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(string username, string password)
+        {
+            Result result = new Result();
+            Thread.Sleep(2000);
+            if (string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                result.Error = "BadRequest";
+                result.Message = "Kullanıcı Adı ve Şifre boş geçilemez!";
+                result.Success = false;
+                return Ok(new { result });
+            }
+
+            if (username == "test" && password == "1234")
+            {
+                result.Error = "";
+                result.Message = "Success";
+                result.Success = true;
+                result.Data = Guid.NewGuid().ToString();
+                return Ok(new { result });
+            }
+
+            result.Error = "Unauthorized";
+            result.Message = "Kullanıcı adı veya şifre yanlış!";
+            result.Success = false;
+            return Ok(new { result });
+        }
+    }
+
+    public class Result
+    {
+        public bool Success { get; set; }
+        public string Error { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+    }
+}
